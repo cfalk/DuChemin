bbc88f73d0d9f0ef2886fb8c616d7f171d3be4a4
Andrew Hankinson <andrew.hankinson@gmail.com>
2012-09-14 04:57:06 -0400

    Adding more admin

--- duchemin/admin/admin.py
+++ duchemin/admin/admin.py
@@ -11,10 +11,12 @@ from duchemin.models.reconstruction import DCReconstruction
 class DCAnalysisAdmin(admin.ModelAdmin):
     list_display = ['analyst', 'composition_number', 'phrase_number', 'start_measure', 'stop_measure']
 
+class DCPhraseAdmin(admin.ModelAdmin):
+    list_display = ['phrase_id', 'piece_id', 'phrase_num', 'phrase_start', 'phrase_stop', 'phrase_text']
 
 admin.site.register(DCAnalysis, DCAnalysisAdmin)
 admin.site.register(DCBook)
 admin.site.register(DCPerson)
-admin.site.register(DCPhrase)
+admin.site.register(DCPhrase, DCPhraseAdmin)
 admin.site.register(DCPiece)
 admin.site.register(DCReconstruction)

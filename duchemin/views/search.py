a7c492e4d847e7cae94abab620a8cb139b7f0a07
Andrew Hankinson <andrew.hankinson@gmail.com>
2012-09-14 21:14:58 -0400

    Updating search functionality

--- duchemin/views/search.py
+++ duchemin/views/search.py
@@ -4,6 +4,7 @@ from django.conf import settings
 
 import solr
 
+
 def search(request):
     s = solr.Solr(settings.SOLR_SERVER)
     ret = s.select("*:*", facet="true", facet_field=['contributor', 'composer', 'is_cadence', 'cadence_final_tone', 'cadence_alter', 'cadence_kind', 'text_treatment', 'repeat_kind', 'book_title'], rows="0")
@@ -20,20 +21,63 @@ def search(request):
     text_treatment = sorted(facets['text_treatment'])
 
     data = {
-        'book_title': book_title,
-        'cadence_alter': cadence_alter,
-        'cadence_final_tone': cadence_final_tone,
-        'cadence_kind': cadence_kind,
-        'composer': composer,
-        'contributor': contributor,
-        'is_cadence': is_cadence,
-        'repeat_kind': repeat_kind,
-        'text_treatment': text_treatment
+        'book_titles': book_title,
+        'cadence_alters': cadence_alter,
+        'cadence_final_tones': cadence_final_tone,
+        'cadence_kinds': cadence_kind,
+        'composers': composer,
+        'contributors': contributor,
+        'is_cadences': is_cadence,
+        'repeat_kinds': repeat_kind,
+        'text_treatments': text_treatment
     }
 
     return render(request, 'search/search.html', data)
 
 
 def query(request):
-    print request
-    return render(request, 'search/results.html')
+    s = solr.Solr(settings.SOLR_SERVER)
+
+    qstring = request.GET.get('q', '*:*')
+    books = request.GET.getlist('b', '')
+    composers = request.GET.getlist('p', '')
+    contributors = request.GET.getlist('r', '')
+    final_tone = request.GET.getlist('f', '')
+    cadence_kind = request.GET.getlist('k', '')
+    cadence_alter = request.GET.getlist('m', '')
+    text_tone = request.GET.getlist('t', '')
+    repeat_kinds = request.GET.getlist('lf', '')
+
+    filter_queries = []
+    if books:
+        filter_queries.append(u"book_title:({0})".format(u" OR ".join([u"\"{0}\"".format(x) for x in books])))
+    if composers:
+        filter_queries.append(u"composer:({0})".format(u" OR ".join([u"\"{0}\"".format(x) for x in composers])))
+    if contributors:
+        filter_queries.append(u"contributor:({0})".format(u" OR ".join([u"\"{0}\"".format(x) for x in contributors])))
+    if final_tone:
+        filter_queries.append(u"cadence_final_tone:({0})".format(u" OR ".join([u"\"{0}\"".format(x) for x in final_tone])))
+    if cadence_kind:
+        filter_queries.append(u"cadence_kind:({0})".format(u" OR ".join([u"\"{0}\"".format(x) for x in cadence_kind])))
+    if cadence_alter:
+        filter_queries.append(u"cadence_alter:({0})".format(u" OR ".join([u"\"{0}\"".format(x) for x in cadence_alter])))
+    if text_tone:
+        filter_queries.append(u"text_treatment:({0})".format(u" OR ".join([u"\"{0}\"".format(x) for x in text_tone])))
+    if repeat_kinds:
+        filter_queries.append(u"repeat_kind:({0})".format(u" OR ".join([u"\"{0}\"".format(x) for x in repeat_kinds])))
+
+    ret = s.select(qstring, fq=filter_queries)
+
+    res = [SolrResponseObject(**s) for s in ret]
+
+    data = {
+        'query': qstring,
+        'results': res
+    }
+
+    return render(request, 'search/results.html', data)
+
+
+class SolrResponseObject(object):
+    def __init__(self, **entries):
+        self.__dict__.update(entries)

b585fe1412589db3efde4b4b86060d4b8b8400c9
Andrew Hankinson <andrew.hankinson@gmail.com>
2012-09-18 20:46:29 -0400

    Update user logins for the admin interface

--- duchemin/admin/admin.py
+++ duchemin/admin/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 from duchemin.models.analysis import DCAnalysis
 from duchemin.models.book import DCBook
@@ -6,13 +8,28 @@ from duchemin.models.person import DCPerson
 from duchemin.models.phrase import DCPhrase
 from duchemin.models.piece import DCPiece
 from duchemin.models.reconstruction import DCReconstruction
+from duchemin.models.userprofile import DCUserProfile
 
 
 class DCAnalysisAdmin(admin.ModelAdmin):
     list_display = ['analyst', 'composition_number', 'phrase_number', 'start_measure', 'stop_measure']
 
+
 class DCPhraseAdmin(admin.ModelAdmin):
     list_display = ['phrase_id', 'piece_id', 'phrase_num', 'phrase_start', 'phrase_stop', 'phrase_text']
+
+
+class UserProfileInline(admin.StackedInline):
+    model = DCUserProfile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+
+class UserAdmin(UserAdmin):
+    inlines = (UserProfileInline, )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 admin.site.register(DCAnalysis, DCAnalysisAdmin)
 admin.site.register(DCBook)

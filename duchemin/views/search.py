49bc45f5dbcbf57e9731bad07373afbd0e63e7df
Andrew Hankinson <andrew.hankinson@mail.mcgill.ca>
2012-10-16 10:03:16 -0400

    Pagination work

--- duchemin/views/search.py
+++ duchemin/views/search.py
@@ -1,188 +1,174 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout, models
+from django.shortcuts import render
+# from django.contrib.auth import authenticate, login, logout, models
 from django.conf import settings
 
 import solr
+import math
 from duchemin.helpers.solrsearch import DCSolrSearch
 
 VOICE_NAMES = ["S", "Ct", "T", "B", "None"]
 
 def search(request):
     data = {}
-    query = None
-
     if 'q' not in request.GET:
-        s = solr.Solr(settings.SOLR_SERVER)
-        ret = s.select("*:*", facet="true",
-            facet_field=['contributor', 'composer', 'is_cadence', 'cadence_final_tone',
-            'cadence_alter', 'cadence_kind', 'text_treatment', 'repeat_kind', 'book_id_title'],
-            fq=['duchemin_analysis'],
-            rows="0")
-
-        facets = ret.facet_counts['facet_fields']
-        cadence_alter = sorted(facets['cadence_alter'])
-        book_title = sorted(facets['book_id_title'])
-        cadence_final_tone = sorted(facets['cadence_final_tone'])
-        cadence_kind = sorted(facets['cadence_kind'])
-        composer = sorted(facets['composer'])
-        contributor = sorted(facets['contributor'])
-        is_cadence = sorted(facets['is_cadence'])
-        repeat_kind = sorted(facets['repeat_kind'])
-        text_treatment = sorted(facets['text_treatment'])
-
-        book_info = sorted([tuple(book.split("_")) for book in book_title])
-
-        cad_voice_cantz = [__construct_voice_facet(v, 'cadence_role_cantz', hidden=False) for v in VOICE_NAMES]
-        cad_voice_tenz = [__construct_voice_facet(v, 'cadence_role_tenz') for v in VOICE_NAMES]
-
-        intpatt_up6_voice = [__construct_voice_facet(v, 'intpatt_p6_up', hidden=False) for v in VOICE_NAMES]
-        intpatt_lo6_voice = [__construct_voice_facet(v, 'intpatt_p6_lo') for v in VOICE_NAMES]
-
-        intpatt_up3_voice = [__construct_voice_facet(v, 'intpatt_p3_up', hidden=False) for v in VOICE_NAMES]
-        intpatt_lo3_voice = [__construct_voice_facet(v, 'intpatt_p3_lo') for v in VOICE_NAMES]
-
-        intpatt_up53_voice = [__construct_voice_facet(v, 'intpatt_53_up', hidden=False) for v in VOICE_NAMES]
-        intpatt_lo53_voice = [__construct_voice_facet(v, 'intpatt_53_lo') for v in VOICE_NAMES]
-
-        prestype_nonim_up1 = [__construct_voice_facet(v, 'prestype_nim_up1', hidden=False) for v in VOICE_NAMES]
-        prestype_nonim_lo1 = [__construct_voice_facet(v, 'prestype_nim_lo1') for v in VOICE_NAMES]
-        prestype_nonim_up2 = [__construct_voice_facet(v, 'prestype_nim_up2') for v in VOICE_NAMES]
-        prestype_nonim_lo2 = [__construct_voice_facet(v, 'prestype_nim_lo2') for v in VOICE_NAMES]
-
-        prestype_free_dux = [__construct_voice_facet(v, 'prestype_free_dux', hidden=False) for v in VOICE_NAMES]
-        prestype_free_comes = [__construct_voice_facet(v, 'prestype_free_comes') for v in VOICE_NAMES]
-
-        prestype_imduet_dux1 = [__construct_voice_facet(v, 'prestype_imduet_dux1', hidden=False) for v in VOICE_NAMES]
-        prestype_imduet_comes1 = [__construct_voice_facet(v, 'prestype_imduet_comes1') for v in VOICE_NAMES]
-        prestype_imduet_dux2 = [__construct_voice_facet(v, 'prestype_imduet_dux2') for v in VOICE_NAMES]
-        prestype_imduet_comes2 = [__construct_voice_facet(v, 'prestype_imduet_comes2') for v in VOICE_NAMES]
-
-        prestype_entry_dux1 = [__construct_voice_facet(v, 'prestype_entry_p_dux1', hidden=False) for v in VOICE_NAMES]
-        prestype_entry_comes1 = [__construct_voice_facet(v, 'prestype_entry_p_comes1') for v in VOICE_NAMES]
-        prestype_entry_dux2 = [__construct_voice_facet(v, 'prestype_entry_p_dux2') for v in VOICE_NAMES]
-        prestype_entry_comes2 = [__construct_voice_facet(v, 'prestype_entry_p_comes2') for v in VOICE_NAMES]
-
-        prestype_entry_t_dux1 = [__construct_voice_facet(v, 'prestype_entry_t_dux1', hidden=False) for v in VOICE_NAMES]
-        prestype_entry_t_comes1 = [__construct_voice_facet(v, 'prestype_entry_t_comes1') for v in VOICE_NAMES]
-        prestype_entry_t_dux2 = [__construct_voice_facet(v, 'prestype_entry_t_dux2') for v in VOICE_NAMES]
-        prestype_entry_t_comes2 = [__construct_voice_facet(v, 'prestype_entry_t_comes2') for v in VOICE_NAMES]
-
-        prestype_entry_s_dux1 = [__construct_voice_facet(v, 'prestype_entry_s_dux1', hidden=False) for v in VOICE_NAMES]
-        prestype_entry_s_comes1 = [__construct_voice_facet(v, 'prestype_entry_s_comes1') for v in VOICE_NAMES]
-        prestype_entry_s_dux2 = [__construct_voice_facet(v, 'prestype_entry_s_dux2') for v in VOICE_NAMES]
-        prestype_entry_s_comes2 = [__construct_voice_facet(v, 'prestype_entry_s_comes2') for v in VOICE_NAMES]
-
-        data = {
-            'book_info': book_info,
-            'cadence_alters': cadence_alter,
-            'cadence_final_tones': cadence_final_tone,
-            'cadence_kinds': cadence_kind,
-            'composers': composer,
-            'contributors': contributor,
-            'is_cadences': is_cadence,
-            'repeat_kinds': repeat_kind,
-            'text_treatments': text_treatment,
-            'cad_voice_cantz': cad_voice_cantz,
-            'cad_voice_tenz': cad_voice_tenz,
-            'intpatt_up6_voice': intpatt_up6_voice,
-            'intpatt_lo6_voice': intpatt_lo6_voice,
-            'intpatt_up3_voice': intpatt_up3_voice,
-            'intpatt_lo3_voice': intpatt_lo3_voice,
-            'intpatt_up53_voice': intpatt_up53_voice,
-            'intpatt_lo53_voice': intpatt_lo53_voice,
-            'prestype_nonim_up1': prestype_nonim_up1,
-            'prestype_nonim_lo1': prestype_nonim_lo1,
-            'prestype_nonim_up2': prestype_nonim_up2,
-            'prestype_nonim_lo2': prestype_nonim_lo2,
-            'prestype_free_dux': prestype_free_dux,
-            'prestype_free_comes': prestype_free_comes,
-            'prestype_imduet_dux1': prestype_imduet_dux1,
-            'prestype_imduet_comes1': prestype_imduet_comes1,
-            'prestype_imduet_dux2': prestype_imduet_dux2,
-            'prestype_imduet_comes2': prestype_imduet_comes2,
-            'prestype_entry_dux1': prestype_entry_dux1,
-            'prestype_entry_comes1': prestype_entry_comes1,
-            'prestype_entry_dux2': prestype_entry_dux2,
-            'prestype_entry_comes2': prestype_entry_comes2,
-            'prestype_entry_t_dux1': prestype_entry_t_dux1,
-            'prestype_entry_t_comes1': prestype_entry_t_comes1,
-            'prestype_entry_t_dux2': prestype_entry_t_dux2,
-            'prestype_entry_t_comes2': prestype_entry_t_comes2,
-            'prestype_entry_s_dux1': prestype_entry_s_dux1,
-            'prestype_entry_s_comes1': prestype_entry_s_comes1,
-            'prestype_entry_s_dux2': prestype_entry_s_dux2,
-            'prestype_entry_s_comes2': prestype_entry_s_comes2
-        }
-        return render(request, 'search/search.html', data)
-
+        return _empty_search(request)
     else:
+        start = request.GET.get('start')
+        numResult = request.GET.get('numResult')
+
+        print start
+        print numResult
+
         s = DCSolrSearch()
         # fetch the number of results, but not the results themselves.
-
         num_res = s.num_results(request, group=['title'], filter=['type:duchemin_analysis'], rows=0)
-        work_res = s.search(request, group=['title'], filter=['type:duchemin_analysis'])
-        el_res = s.search(request, filter=['type:duchemin_analysis'])
+        work_res = s.search(request, group=['title'], filter=['type:duchemin_analysis'], start=start)
+        el_res = s.search(request, filter=['type:duchemin_analysis'], start=start)
 
-        print num_res.grouped
+        print dir(num_res)
 
         work_results = [SolrResponseObject(**s) for s in work_res.results]
         element_results = [SolrResponseObject(**s) for s in el_res.results]
         work_result_num = num_res.grouped['title']['ngroups']
         element_result_num = num_res.grouped['title']['matches']
+
+        work_paginate = False
+        element_paginate = False
+        num_work_pages = 0
+        num_element_pages = 0
+
+        if int(work_result_num) > 10:
+            work_paginate = True
+            num_work_pages = range(1, int(math.ceil(work_result_num / 10)) + 1)
+
+        if int(element_result_num) > 10:
+            element_paginate = True
+            num_element_pages = range(1, int(math.ceil(element_result_num / 10)) + 1)
+
         data = {
-            'query': "Foo",
+            # 'work_res_query': work_res_query,
+            # 'el_res_query': el_res_query,
             'work_results': work_results,
             'element_results': element_results,
             'work_result_num': work_result_num,
             'element_result_num': element_result_num,
-            'render_notation': True
+            'render_notation': True,
+            'work_paginate': work_paginate,
+            'element_paginate': element_paginate,
+            'start': start,
+            'numResult': numResult,
+            'num_work_pages': num_work_pages,
+            'num_element_pages': num_element_pages
         }
+
         return render(request, 'search/results.html', data)
 
 
-def query(request):
-    s = DCSolrSearch()
-    s.map_query(request)
-    # s = solr.Solr(settings.SOLR_SERVER)
+def _empty_search(request):
+    s = solr.Solr(settings.SOLR_SERVER)
+    ret = s.select("*:*", facet="true",
+        facet_field=['contributor', 'composer', 'is_cadence', 'cadence_final_tone',
+        'cadence_alter', 'cadence_kind', 'text_treatment', 'repeat_kind', 'book_id_title'],
+        fq=['duchemin_analysis'],
+        rows="0")
 
-    # qstring = request.GET.get('q', '*:*')
-    # books = request.GET.getlist('b', '')
-    # composers = request.GET.getlist('p', '')
-    # contributors = request.GET.getlist('r', '')
-    # final_tone = request.GET.getlist('f', '')
-    # cadence_kind = request.GET.getlist('k', '')
-    # cadence_alter = request.GET.getlist('m', '')
-    # text_tone = request.GET.getlist('t', '')
-    # repeat_kinds = request.GET.getlist('lf', '')
+    facets = ret.facet_counts['facet_fields']
+    cadence_alter = sorted(facets['cadence_alter'])
+    book_title = sorted(facets['book_id_title'])
+    cadence_final_tone = sorted(facets['cadence_final_tone'])
+    cadence_kind = sorted(facets['cadence_kind'])
+    composer = sorted(facets['composer'])
+    contributor = sorted(facets['contributor'])
+    is_cadence = sorted(facets['is_cadence'])
+    repeat_kind = sorted(facets['repeat_kind'])
+    text_treatment = sorted(facets['text_treatment'])
 
-    # filter_queries = []
-    # if books:
-    #     filter_queries.append(u"book_title:({0})".format(u" OR ".join([u"\"{0}\"".format(x) for x in books])))
-    # if composers:
-    #     filter_queries.append(u"composer:({0})".format(u" OR ".join([u"\"{0}\"".format(x) for x in composers])))
-    # if contributors:
-    #     filter_queries.append(u"contributor:({0})".format(u" OR ".join([u"\"{0}\"".format(x) for x in contributors])))
-    # if final_tone:
-    #     filter_queries.append(u"cadence_final_tone:({0})".format(u" OR ".join([u"\"{0}\"".format(x) for x in final_tone])))
-    # if cadence_kind:
-    #     filter_queries.append(u"cadence_kind:({0})".format(u" OR ".join([u"\"{0}\"".format(x) for x in cadence_kind])))
-    # if cadence_alter:
-    #     filter_queries.append(u"cadence_alter:({0})".format(u" OR ".join([u"\"{0}\"".format(x) for x in cadence_alter])))
-    # if text_tone:
-    #     filter_queries.append(u"text_treatment:({0})".format(u" OR ".join([u"\"{0}\"".format(x) for x in text_tone])))
-    # if repeat_kinds:
-    #     filter_queries.append(u"repeat_kind:({0})".format(u" OR ".join([u"\"{0}\"".format(x) for x in repeat_kinds])))
+    book_info = sorted([tuple(book.split("_")) for book in book_title])
 
-    # ret = s.select(qstring, fq=filter_queries)
+    cad_voice_cantz = [__construct_voice_facet(v, 'cadence_role_cantz', hidden=False) for v in VOICE_NAMES]
+    cad_voice_tenz = [__construct_voice_facet(v, 'cadence_role_tenz') for v in VOICE_NAMES]
 
-    # res = [SolrResponseObject(**s) for s in ret]
+    intpatt_up6_voice = [__construct_voice_facet(v, 'intpatt_p6_up', hidden=False) for v in VOICE_NAMES]
+    intpatt_lo6_voice = [__construct_voice_facet(v, 'intpatt_p6_lo') for v in VOICE_NAMES]
 
-    # data = {
-    #     'query': qstring,
-    #     'results': res
-    # }
+    intpatt_up3_voice = [__construct_voice_facet(v, 'intpatt_p3_up', hidden=False) for v in VOICE_NAMES]
+    intpatt_lo3_voice = [__construct_voice_facet(v, 'intpatt_p3_lo') for v in VOICE_NAMES]
 
-    return render(request, 'search/results.html', data)
+    intpatt_up53_voice = [__construct_voice_facet(v, 'intpatt_53_up', hidden=False) for v in VOICE_NAMES]
+    intpatt_lo53_voice = [__construct_voice_facet(v, 'intpatt_53_lo') for v in VOICE_NAMES]
+
+    prestype_nonim_up1 = [__construct_voice_facet(v, 'prestype_nim_up1', hidden=False) for v in VOICE_NAMES]
+    prestype_nonim_lo1 = [__construct_voice_facet(v, 'prestype_nim_lo1') for v in VOICE_NAMES]
+    prestype_nonim_up2 = [__construct_voice_facet(v, 'prestype_nim_up2') for v in VOICE_NAMES]
+    prestype_nonim_lo2 = [__construct_voice_facet(v, 'prestype_nim_lo2') for v in VOICE_NAMES]
+
+    prestype_free_dux = [__construct_voice_facet(v, 'prestype_free_dux', hidden=False) for v in VOICE_NAMES]
+    prestype_free_comes = [__construct_voice_facet(v, 'prestype_free_comes') for v in VOICE_NAMES]
+
+    prestype_imduet_dux1 = [__construct_voice_facet(v, 'prestype_imduet_dux1', hidden=False) for v in VOICE_NAMES]
+    prestype_imduet_comes1 = [__construct_voice_facet(v, 'prestype_imduet_comes1') for v in VOICE_NAMES]
+    prestype_imduet_dux2 = [__construct_voice_facet(v, 'prestype_imduet_dux2') for v in VOICE_NAMES]
+    prestype_imduet_comes2 = [__construct_voice_facet(v, 'prestype_imduet_comes2') for v in VOICE_NAMES]
+
+    prestype_entry_dux1 = [__construct_voice_facet(v, 'prestype_entry_p_dux1', hidden=False) for v in VOICE_NAMES]
+    prestype_entry_comes1 = [__construct_voice_facet(v, 'prestype_entry_p_comes1') for v in VOICE_NAMES]
+    prestype_entry_dux2 = [__construct_voice_facet(v, 'prestype_entry_p_dux2') for v in VOICE_NAMES]
+    prestype_entry_comes2 = [__construct_voice_facet(v, 'prestype_entry_p_comes2') for v in VOICE_NAMES]
+
+    prestype_entry_t_dux1 = [__construct_voice_facet(v, 'prestype_entry_t_dux1', hidden=False) for v in VOICE_NAMES]
+    prestype_entry_t_comes1 = [__construct_voice_facet(v, 'prestype_entry_t_comes1') for v in VOICE_NAMES]
+    prestype_entry_t_dux2 = [__construct_voice_facet(v, 'prestype_entry_t_dux2') for v in VOICE_NAMES]
+    prestype_entry_t_comes2 = [__construct_voice_facet(v, 'prestype_entry_t_comes2') for v in VOICE_NAMES]
+
+    prestype_entry_s_dux1 = [__construct_voice_facet(v, 'prestype_entry_s_dux1', hidden=False) for v in VOICE_NAMES]
+    prestype_entry_s_comes1 = [__construct_voice_facet(v, 'prestype_entry_s_comes1') for v in VOICE_NAMES]
+    prestype_entry_s_dux2 = [__construct_voice_facet(v, 'prestype_entry_s_dux2') for v in VOICE_NAMES]
+    prestype_entry_s_comes2 = [__construct_voice_facet(v, 'prestype_entry_s_comes2') for v in VOICE_NAMES]
+
+    data = {
+        'book_info': book_info,
+        'cadence_alters': cadence_alter,
+        'cadence_final_tones': cadence_final_tone,
+        'cadence_kinds': cadence_kind,
+        'composers': composer,
+        'contributors': contributor,
+        'is_cadences': is_cadence,
+        'repeat_kinds': repeat_kind,
+        'text_treatments': text_treatment,
+        'cad_voice_cantz': cad_voice_cantz,
+        'cad_voice_tenz': cad_voice_tenz,
+        'intpatt_up6_voice': intpatt_up6_voice,
+        'intpatt_lo6_voice': intpatt_lo6_voice,
+        'intpatt_up3_voice': intpatt_up3_voice,
+        'intpatt_lo3_voice': intpatt_lo3_voice,
+        'intpatt_up53_voice': intpatt_up53_voice,
+        'intpatt_lo53_voice': intpatt_lo53_voice,
+        'prestype_nonim_up1': prestype_nonim_up1,
+        'prestype_nonim_lo1': prestype_nonim_lo1,
+        'prestype_nonim_up2': prestype_nonim_up2,
+        'prestype_nonim_lo2': prestype_nonim_lo2,
+        'prestype_free_dux': prestype_free_dux,
+        'prestype_free_comes': prestype_free_comes,
+        'prestype_imduet_dux1': prestype_imduet_dux1,
+        'prestype_imduet_comes1': prestype_imduet_comes1,
+        'prestype_imduet_dux2': prestype_imduet_dux2,
+        'prestype_imduet_comes2': prestype_imduet_comes2,
+        'prestype_entry_dux1': prestype_entry_dux1,
+        'prestype_entry_comes1': prestype_entry_comes1,
+        'prestype_entry_dux2': prestype_entry_dux2,
+        'prestype_entry_comes2': prestype_entry_comes2,
+        'prestype_entry_t_dux1': prestype_entry_t_dux1,
+        'prestype_entry_t_comes1': prestype_entry_t_comes1,
+        'prestype_entry_t_dux2': prestype_entry_t_dux2,
+        'prestype_entry_t_comes2': prestype_entry_t_comes2,
+        'prestype_entry_s_dux1': prestype_entry_s_dux1,
+        'prestype_entry_s_comes1': prestype_entry_s_comes1,
+        'prestype_entry_s_dux2': prestype_entry_s_dux2,
+        'prestype_entry_s_comes2': prestype_entry_s_comes2,
+        'start': 0,
+        'numResult': 10
+    }
+    return render(request, 'search/search.html', data)
 
 
 def __construct_voice_facet(voice, group, hidden=True):

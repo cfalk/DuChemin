dbada80a893a46ba8c3de4b332b600bfa27804a7
Andrew Hankinson <andrew.hankinson@gmail.com>
2012-10-22 22:55:58 -0400

    Add reconstruction model
    For issue #5

--- duchemin/models/reconstruction.py
+++ duchemin/models/reconstruction.py
@@ -1,4 +1,6 @@
 from django.db import models
+from duchemin.models.piece import DCPiece
+from duchemin.models.person import DCPerson
 
 
 class DCReconstruction(models.Model):
@@ -6,3 +8,17 @@ class DCReconstruction(models.Model):
         app_label = "duchemin"
         verbose_name = "Reconstruction"
         verbose_name_plural = "Reconstructions"
+
+    piece = models.ForeignKey(DCPiece)
+    reconstructor = models.ForeignKey(DCPerson)
+
+    def __unicode__(self):
+        return self.reconstruction_id
+
+    @property
+    def recon_id(self):
+        return u"{0}_{1}".format(self.piece.piece_id, self.reconstructor.surname)
+
+    @property
+    def recon_filename(self):
+        return u"{0}_{1}.xml".format(self.piece.piece_id, self.reconstructor.surname)

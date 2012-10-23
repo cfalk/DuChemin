cebc6f0ae0e7374550aedff2739f00c335bf90b0
Andrew Hankinson <andrew.hankinson@gmail.com>
2012-10-23 12:29:18 -0400

    Updating to support multiple favourites

--- duchemin/models/userprofile.py
+++ duchemin/models/userprofile.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from duchemin.models.piece import DCPiece
+from duchemin.models.analysis import DCAnalysis
+from duchemin.models.reconstruction import DCReconstruction
 
 
 class DCUserProfile(models.Model):
@@ -8,4 +10,6 @@ class DCUserProfile(models.Model):
         app_label = "duchemin"
 
     user = models.OneToOneField(User)
-    favourited = models.ManyToManyField(DCPiece)
+    favourited_piece = models.ManyToManyField(DCPiece)
+    favourited_analysis = models.ManyToManyField(DCAnalysis)
+    favourited_reconstruction = models.ManyToManyField(DCReconstruction)

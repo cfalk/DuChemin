c9aaa4a45eb3ec176a40888e97aea2cbe3f3e6a9
Andrew Hankinson <andrew.hankinson@gmail.com>
2012-10-24 01:54:07 -0400

    Add admin functionality for Pieces and Reconstructions

--- duchemin/admin/admin.py
+++ duchemin/admin/admin.py
@@ -9,15 +9,36 @@ from duchemin.models.phrase import DCPhrase
 from duchemin.models.piece import DCPiece
 from duchemin.models.reconstruction import DCReconstruction
 from duchemin.models.userprofile import DCUserProfile
+from duchemin.models.file import DCFile
 
 
 class DCAnalysisAdmin(admin.ModelAdmin):
     list_display = ['analyst', 'composition_number', 'phrase_number', 'start_measure', 'stop_measure']
 
+class DCFilePieceInline(admin.TabularInline):
+    model = DCPiece.attachments.through
+    can_delete = True,
+    verbose_name = "File"
+    verbose_name_plural = "Files"
+
+class DCPieceAdmin(admin.ModelAdmin):
+    inlines = (
+        DCFilePieceInline,
+    )
+
+class DCFileReconstructionInline(admin.TabularInline):
+    model = DCReconstruction.attachments.through
+    can_delete = True
+    verbose_name = "File"
+    verbose_name_plural = "Files"
+
+class DCReconstructionAdmin(admin.ModelAdmin):
+    inlines = (
+        DCFileReconstructionInline,
+    )
 
 class DCPhraseAdmin(admin.ModelAdmin):
     list_display = ['phrase_id', 'piece_id', 'phrase_num', 'phrase_start', 'phrase_stop', 'phrase_text']
-
 
 class UserProfileInline(admin.StackedInline):
     model = DCUserProfile
@@ -35,5 +56,6 @@ admin.site.register(DCAnalysis, DCAnalysisAdmin)
 admin.site.register(DCBook)
 admin.site.register(DCPerson)
 admin.site.register(DCPhrase, DCPhraseAdmin)
-admin.site.register(DCPiece)
-admin.site.register(DCReconstruction)
+admin.site.register(DCPiece, DCPieceAdmin)
+admin.site.register(DCReconstruction, DCReconstructionAdmin)
+admin.site.register(DCFile)

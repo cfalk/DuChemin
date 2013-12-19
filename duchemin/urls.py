d942f971c2332b9abd5e9e354b684c6e6f13fe72
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-12-19 13:38:31 -0500

    Updated urls for new views

--- duchemin/urls.py
+++ duchemin/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
-
+from duchemin.views.analysis import AnalysisList, AnalysisDetail
+from duchemin.views.person import PersonList, PersonDetail
+from duchemin.views.phrase import PhraseList, PhraseDetail
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -15,19 +17,26 @@ if 'django.contrib.admin' in settings.INSTALLED_APPS:
     urlpatterns += patterns('duchemin.views.main',
         url(r'^$', 'home', name='home'),
         url(r'^piece/(?P<piece_id>[0-9a-zA-Z]+)/add-observation/$', 'add_observation'),
-        url(r'^piece/(?P<piece_id>[0-9a-zA-Z]+)', 'piece'),
-        url(r'^pieces/$', 'pieces'),
+        url(r'^piece/(?P<pk>[0-9a-zA-Z]+)', 'piece', name="dcpiece-detail"),
+        url(r'^pieces/$', 'pieces', name="dcpiece-list"),
 
-        url(r'^book/(?P<book_id>[0-9]+)', 'book'),
-        url(r'^books/$', 'books'),
+        url(r'^book/(?P<pk>[0-9]+)', 'book', name="dcbook-detail"),
+        url(r'^books/$', 'books', name="dcbook-list"),
 
         url(r'^profile/', 'profile'),
 
         url(r'^reconstructions/$', 'reconstructions'),
         url(r'^reconstruction/(?P<reconstruction_id>[0-9]+)', 'reconstruction'),
 
-        url(r'^people/$', 'people'),
-        url(r'^person/(?P<person_id>[a-zA-Z0-9]+)', 'person'),
+        url(r'^people/$', PersonList.as_view(), name="dcperson-list"),
+        url(r'^person/(?P<pk>[a-zA-Z0-9]+)', PersonDetail.as_view(), name="dcperson-detail"),
+
+        url(r'^analyses/$', AnalysisList.as_view(), name="dcanalysis-list"),
+        url(r'^analysis/(?P<pk>[0-9]+)/$', AnalysisDetail.as_view(), name="dcanalysis-detail"),
+
+        url(r'^phrases/$', PhraseList.as_view(), name="dcphrase-list"),
+        url(r'^phrase/(?P<pk>[0-9]+)/$', PhraseDetail.as_view(), name="dcphrase-detail"),
+
     )
 
     urlpatterns += patterns('duchemin.views.search',
